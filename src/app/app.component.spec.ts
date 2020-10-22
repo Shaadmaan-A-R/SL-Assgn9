4b8693b39e210f682b68cc4fa4f4a5c1ce644db4
Rahul Kumar Sharma <rahulkumarsharma182000@gmail.com>
2020-10-22 12:01:56 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sllab9'`, () => {
+  it(`should have as title 'todoLab9'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sllab9');
+    expect(app.title).toEqual('todoLab9');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sllab9 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('todoLab9 app is running!');
   });
 });
